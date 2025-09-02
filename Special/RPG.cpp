9401ec725401f30bb46f17a9fbdb138be2833905
Prithvi <prithvikulahalli@gmail.com>
2025-09-02 19:40:43 +0530

    Update RPG.cpp

--- Special/RPG.cpp
+++ Special/RPG.cpp
@@ -3,64 +3,49 @@
 
 using namespace std;
 
+class FinalForm {
+private:
+  int health;
+  string weapon, shield, galaxyBoots;
 
-class FinalForm{
-    private:
-        int health;
-        string weapon;
-        string shield;
-        string galaxyBoots;
-    public:
-        FinalForm(int h, string w, string s, string gb){
-            this->health = h;
-            this->weapon = w;
-            this->shield = s;
-            this->galaxyBoots = gb;
-        }
-        void attack(){
-            cout << "Attacking with a sword\n";
-        }
-        void defend(){
-            cout << "Defended a blow\n";
-        }
-        void run(){
-            cout << "Sprinting\n";
-        }
+public:
+  FinalForm(int h, string w, string s, string gb)
+      : health(h), weapon(w), shield(s), galaxyBoots(gb) {}
+  ~FinalForm() {};
+  void attack() { cout << "Attacking with a sword\n"; }
+  void defend() { cout << "Defended a blow\n"; }
+  void run() { cout << "Sprinting\n"; }
 };
 
-class Tools{
-    public:
-        string shield = "Iron";
-        string galaxyBoots = "Galaxy";
+class Tools {
+public:
+  string shield = "Iron";
+  string galaxyBoots = "Galaxy";
 };
 
-class legacyHero{
-    private:
-        int health;
-        string weapon;
-    public:
-        legacyHero(int h, string w){
-            this->health = h;
-            this->weapon = w;
-        }
-        void attack(){
-            cout << "Attacking with a sword\n";
-        }
-        FinalForm operator + (Tools& t){
-            return FinalForm(this->health, this->weapon, t.shield, t.galaxyBoots);
-        }
+class legacyHero {
+private:
+  int health;
+  string weapon;
+
+public:
+  legacyHero(int h, string w) : health(h), weapon(w) {}
+  ~legacyHero() {};
+  void attack() { cout << "Attacking with a sword\n"; }
+  FinalForm operator+(Tools &t) {
+    return FinalForm(this->health, this->weapon, t.shield, t.galaxyBoots);
+  }
 };
 
-
-int main(){
-    int level = 10;
-    legacyHero hero(100, "sword");
-    if (level >= 10) {
-        cout << "You got an upgrade!!!\n";
-        Tools t;
-        FinalForm finalHero = hero + t;
-        finalHero.attack();
-        finalHero.defend();
-        finalHero.run();
-    }
+int main() {
+  int level = 10;
+  legacyHero hero(100, "sword");
+  if (level >= 10) {
+    cout << "You got an upgrade!!!\n";
+    Tools t;
+    FinalForm finalHero = hero + t;
+    finalHero.attack();
+    finalHero.defend();
+    finalHero.run();
+  }
 }
